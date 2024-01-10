7c272048ea05ab59b99a0ec1ae44bf1fd8b44606
naisei4 <156180475+naisei4@users.noreply.github.com>
2024-01-10 19:51:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if mode == "***回答***":
           service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="""
           {テーマ} = JR東日本の旅客営業規則 
           
-          あなたは{テーマ}の専門家です。クライアントの質問に対して簡潔に説明し、それに関する4択の質問を出してください。
+          あなたは{テーマ}の専門家です。提供したデータに基づいて回答してください。
           """))
           st.text("うぇかぴぽ")
           index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
