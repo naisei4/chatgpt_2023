802c6e34ac6bba5c70450e66a02f3d1b72c999b6
naisei4 <156180475+naisei4@users.noreply.github.com>
2024-01-15 19:06:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,7 @@
-import csv
-from pathlib import Path
-import requests
-import codecs
 import streamlit as st
-from llama_index import GPTVectorStoreIndex, ServiceContext, Document, StorageContext, load_index_from_storage
-from llama_index import download_loader
+from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-
-SimpleWebPageReader = download_loader("SimpleWebPageReader", custom_path="local_dir")
-class ShiftJISToUTF8WebPageReader(SimpleWebPageReader):
-    def load_data(self, urls):
-        data = []
-        for url in urls:
-            response = requests.get(url)
-            response.encoding = "Shift_JIS"
-            content = response.content
-            utf8_content = codecs.decode(content, "shift-jis", "ignore").encode("utf-8", "ignore").decode("utf-8")
-            doc = Document(text=utf8_content)
-            data.append(doc)
-        return data
-
-def is_shift_jis(response):
-    content = response.text
-    if 'charset=Shift_JIS' in content:
-        st.text("Shift_jis変換します")
-        return True
-    else:
-        return False
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
